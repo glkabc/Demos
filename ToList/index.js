5616efdbe283705e808e0ed9055893521c54dbdc
TODAY_ONE <1614696693@qq.com>
2020-12-01 18:01:41 +0800

    1

---
+++ ToList/index.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('.input input');
+const add = document.querySelector('.add');
+const itemList = document.querySelector('.item');
+const showAllButton = document.querySelector('.all');
+const showFinish = document.querySelector('.finish');
+const showUnfinished = document.querySelector('.unfinished');
+
